ff28e08f82f41d43cdcc7f28f7a1fe3959f86496
Justin Dowty <justindowty777@hotmail.com>
2018-11-29 08:47:32 -0500

    good to go

--- CIS343/javascrpt_proj/oogle-api/OoGle-people-manager.js
+++ CIS343/javascrpt_proj/oogle-api/OoGle-people-manager.js
@@ -11,6 +11,8 @@ const fs = require('fs');
 const database = require('./programmers.json');
 // Creating array instance of database objects
 const db = [database];
+// Getting db keys
+const dbKeys = Object.keys(database);
 
 // Make an instance of our express application
 const app = express();
@@ -18,7 +20,21 @@ const app = express();
 const port = 3000;
 
 // Apply our middleware so our code can natively handle JSON easily
+
 app.use(bodyParser.json());
+
+/**************************************/
+/* Adding additional middleware to gracefully handle JSON errors */
+/* Adapted from: https://stackoverflow.com/questions/40142928/how-do-you-reject-an-invalid-json-body-using-express-or-body-parser */
+/**************************************/
+app.use((err, req, res, next) => {
+	console.log("Improper JSON Request!");
+  // body-parser will set this to 400 if the json is in error
+  if(err.status === 400){
+    return res.status(err.status).send("Hey man, that isn't JSON");
+  }
+  return next(err); // if it's not a 400, let the default error handling do it. 
+});
 
 // We must have our list of programmers to use
 if (!fs.existsSync('./programmers.json')) {
@@ -27,28 +43,68 @@ if (!fs.existsSync('./programmers.json')) {
 
 // Build our routes
 
+// Send the database
 app.get('/', (req, res) => {
   res.send(db);
 });
 
+// Send all users with SID
 app.get('/:id', (req, res) => {
   const id = req.params.id;
-  res.send(db.find(item => item.firstName === id));
+  var list = [];
+  db.forEach(e => {
+	if(e.SID === id){
+       list.push(e);
+	}
+  });
+  /* If id is not found, send Not Found */
+  if(list.length == 0){
+	res.sendStatus(404);
+  } else {
+    res.send(list);
+  }
 });
 
 app.put('/:id', (req, res) => {
   const id = req.params.id;
-  res.send(`Fill me in to update values with ID: ${id}`);
+  const body = req.body;  
+  var i = db.length - 1;
+  /* Updating fields of items with PUT request ID */
+  console.log("ID", id);
+  while(i >= 0){
+	console.log(db[i].SID);
+	if(db[i].SID === id){
+      dbKeys.forEach(key => {
+		if(body[key]){
+		  db[i][key] = body[key];
+		} else {}
+	  });  
+	}
+	i--;
+  }
+  res.send(`Updated values with ID: ${id}`);
 });
 
 app.post('/', (req, res) => {
-  const body = req.body; // Hold your JSON in here!
-  db.add(body);
-  console.log(db);
-  res.send(`You sent: ${body}`);
+  const body = req.body;
+  const bodyKeys = Object.keys(body);
+  /* Putting data into appropriate structure */
+  let data = {};
+  dbKeys.forEach(key => {
+	if(body[key]){
+      data[key] = body[key];
+	} else {
+	  data[key] = "";
+	}
+  }); 
+  db.push(data);
+  res.send(`You sent: ${JSON.stringify(body)}`);
 });
 
 // IMPLEMENT A ROUTE TO HANDLE ALL OTHER ROUTES AND RETURN AN ERROR MESSAGE
+app.all('*', (req, res) => {
+  res.sendStatus(403);
+});
 
 app.listen(port, () => {
   console.log(`She's alive on port ${port}`);
